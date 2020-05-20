9658734459091d2a25ab6a79e60b50627bd527f2
Iheb dhouibi <iheb.dhouibi@polytechnicien.tn>
2020-05-20 14:15:12 +0200

    Setting up express js server

---
+++ Server/src/app.js
@@ -0,0 +1,25 @@
+// creating enviroment variable to store our packages 
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const morgan = require("morgan")
+
+const app = express()
+// body-parser allows you to process json data easily
+app.use(bodyParser.json())
+// print logs in a certain way
+app.use(morgan("combined"))
+// managing servers and their accessibility, enabling it may causes security issues 
+app.use(cors())
+
+
+// create and end-point 
+app.get("/status", (req , res) =>{
+   res.send({
+       message: "Hello world !"
+   })
+})
+
+
+
+app.listen(process.env.PORT || 8081)
